cc3ed897e98156046156a3728b8568cc9f0a164d
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-08-05 16:30:12 -0500

    completed functioning shopping list in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Greeter from './components/Greeter';
 import ShoppingList from './components/ShoppingList';
 import Item from './models/item';
 import ShoppingListForm from './components/ShoppingListForm';
+import {v4 as getId} from "uuid"
 
 function App() {
   const [items, setItems ] = useState<Item[]>([])
-  const addItem = (product: string) => {
+  const addItem = (product: string, quantity: number) => {
     console.log("Made it to app component")
     console.log(product)
+    setItems([...items, {id: getId(), product, quantity}])
   }
   return (
     <div>
--- src/components/ShoppingListForm.tsx
+++ src/components/ShoppingListForm.tsx
@@ -1,21 +1,25 @@
 import React, { useRef } from 'react'
 
 interface ShoppingListFormProps {
-    addItem: (item: string) => void
+    addItem: (item: string, quantity: number) => void
 }
 
 function ShoppingListForm({addItem}: ShoppingListFormProps): JSX.Element {
-    const inputRef = useRef<HTMLInputElement>(null)
+    const productInputRef = useRef<HTMLInputElement>(null)
+    const quantityInputRef = useRef<HTMLInputElement>(null)
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
-    const newProduct = inputRef.current!.value
-    addItem(newProduct)
-    inputRef.current!.value = ""
+    const newProduct = productInputRef.current!.value
+    const newQuantity = Number(quantityInputRef.current!.value)
+    addItem(newProduct, newQuantity)
+    productInputRef.current!.value = ""
+    quantityInputRef.current!.value = "1"
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input ref={inputRef} type="text" placeholder="Product Name" />
+        <input ref={productInputRef} type="text" placeholder="Product Name" />
+        <input type="number" min={0} ref={quantityInputRef}/>
         <button type="submit">Add Item</button>
       </form>
     </div>
