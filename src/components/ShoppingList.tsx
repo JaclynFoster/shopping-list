dd3c493e168e5e020fec9d071f9128d2a99c8d3c
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-08-05 15:28:36 -0500

    moved interface to models folder and added state array

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
+import Item from '../models/item';
 
-type Item = {
-  id: number,
-  product: string,
-  quantity: number
-}
 
 interface ShoppingListProps {
   items: Item[];
